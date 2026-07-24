4e7c6e1c77ad2d3d06fe6229223fe330576821c5
smeet7305 <f20230896@goa.bits-pilani.ac.in>
2026-07-25 02:18:36 +0530

    Add: number-of-provinces - submission-3

---
+++ Data Structures & Algorithms/number-of-provinces/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def dfs(self,isConnected,visited,node):
+        visited.add(node)
+        for neigh in range(len(isConnected)):
+            if isConnected[node][neigh]==1 and neigh not in visited:   
+                self.dfs(isConnected,visited,neigh)
+
+               
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n=len(isConnected)
+        visited=set()
+        province=0
+        for node in range(n):
+            if node not in visited:
+                self.dfs(isConnected,visited,node)
+                province+=1 
+        return province
+
+
+
+
+
