0f8b0b36f5f21e5d4b7386be3f0bac865fdcf356
smeet7305 <f20230896@goa.bits-pilani.ac.in>
2026-08-17 14:35:34 +0530

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Solution:
+    def dfs(self,node,visited,pathvisited,adj,st):
+        visited.add(node)
+        pathvisited.add(node)
+        for neigh in adj[node]:
+            if neigh not in visited:
+                if self.dfs(neigh,visited,pathvisited,adj,st):
+                    return True
+            elif neigh in visited and neigh in pathvisited:
+                return True
+        
+        pathvisited.remove(node)
+        st.append(node)
+        return False
+
+
+
+
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj=defaultdict(list)
+        for u,v in prerequisites:
+            adj[v].append(u)
+        
+        visited=set()
+        pathvisited=set()
+        st=[]
+        for i in range(numCourses):
+            if i not in visited:
+                if self.dfs(i,visited,pathvisited,adj,st):
+                    return []
+        return st[::-1]
